38b911abc38b55e1fd3a38277b7a30255b505387
vishwanath <101391216+viswa84@users.noreply.github.com>
2024-03-06 01:10:58 +0530

    jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc register a user
 //@route Post /api/users/register
@@ -25,9 +26,9 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password: hashedPassword,
   });
-  if(user){
-    res.status(201).json({_id:user.id,email:user.email})
-  }else {
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
     res.status(400);
     throw new Error("USer data us not valid");
   }
@@ -37,7 +38,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route Post /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login the user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All the fields are Manatery");
+  }
+  const user = await User.findOne({ email });
+  if (user && bcrypt.compare(password, user.password)) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "4m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password not valid");
+  }
 });
 
 //@desc register a user
